86d1a62d7a4ba83cfd0fa7015cfac32d1f5749b2
ChrisSarpong <cjs7724@psu.edu>
2025-02-13 13:02:14 -0500

    Card_ transfer

--- src/my-card.js
+++ src/my-card.js
@@ -14,7 +14,9 @@ export class MyCard extends LitElement {
 
   constructor() {
     super();
-    this.title = "My card";
+    this.title = "The Hermit Crab";
+    this.image = "https://upload.wikimedia.org/wikipedia/commons/8/8e/Calliactis_and_Dardanus_001.JPG";
+    this.fancy = false;
   }
 
   static get styles() {
@@ -22,16 +24,100 @@ export class MyCard extends LitElement {
       :host {
         display: block;
       }
+        :host([fancy]) {
+          display: block;
+          background-color: pink;
+          border: 2px solid fuchsia;
+          box-shadow: 10px 5px 5px red;
+      }
+        .card {
+          border-radius: 4px;
+          box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+          overflow: hidden;
+          margin: 16px;
+        }
+        .clone, .name-change, .pic-change{
+          background-color: SaddleBrown;
+          border: 3px solid black;
+          color: white;
+  
+        }
+        .bckr-change, .delete-button{
+          background-color: SaddleBrown;
+          margin: 5px;
+          border: 3px solid black;
+          color: white;
+        }
+        .btn-wrapper {
+         background-color: tan;
+          padding: 10px;
+        }
+        .btn {
+        background-color: SaddleBrown;
+        color: white;
+        font-size: 20px;
+        border-radius: 0%;
+        padding: 14px 40px;
+        width: 80%;
+        margin: 4px 10px 10px 32px;
+        }
+        .btn:focus,.btn:hover {
+        background-color: Moccasin;
+        color: Chocolate;
+        }
+        .card {
+        width: 300px;
+        border: 15px solid darkBlue;
+        padding: 0px;
+        margin: 20px;
+      }
+      h1 {
+  color: Chocolate;
+  margin: 20px
+}
+p {
+  color: SaddleBrown; 
+  margin; 20px
+}
+.change-color{
+  background-color: LightGoldenRodYellow;
+}
+.boarder-color{
+  border: 15px solid DarkSeaGreen;
+}
+
     `;
   }
 
   render() {
-    return html`<div>${this.title}</div>`;
-  }
-
+  return html`
+    <div class="card">
+      <div class="btn-wrapper">
+        <h1 class="card-Title">${this.title}</h1>
+        <p>A hermit crab is a part of the crustacean family and can live up to 10-70 years! They have adapted to abandon their shell and seek out new and better shells based on their needs. This is where they get their name from, as they are constantly growing and always looking for a new home!</p>
+        <a href="https://iam.hax.psu.edu/cjs7724/sites/cblog/article-1">
+          <button class="btn">Learn more here</button>
+        </a>
+        <div id="panel">
+          <button class="clone">Clone Card</button>
+          <button class="name-change">Change name</button>
+          <button class="pic-change">Change picture</button>
+          <button class="bckr-change">Change background</button>
+          <button class="delete-button">Delete button</button>
+        </div>
+        <img class="card-image" src="https://upload.wikimedia.org/wikipedia/commons/8/8e/Calliactis_and_Dardanus_001.JPG" style="width: 280px" />
+      </div>
+    </div>
+    <!-- put this in your render method where you had details -->
+  <details ?open="${this.fancy}" @toggle="${this.openChanged}">
+  `;
+}
+       
   static get properties() {
     return {
       title: { type: String },
+      image: { type: String },
+      fancy: { type: Boolean, reflect: true },
     };
   }
 }
