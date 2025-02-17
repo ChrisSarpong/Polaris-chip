c556a71b1e69a549d486ff1dba7d792a3c947f2d
ChrisSarpong <cjs7724@psu.edu>
2025-02-16 23:55:55 -0500

    card_update

--- src/my-card.js
+++ src/my-card.js
@@ -22,75 +22,83 @@ export class MyCard extends LitElement {
   static get styles() {
     return css`
       :host {
-        display: block;
+      display: block;
       }
-        :host([fancy]) {
-          display: block;
-          background-color: pink;
-          border: 2px solid fuchsia;
-          box-shadow: 10px 5px 5px red;
+      .card {
+      border-radius: 4px;
+      box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+      overflow: hidden;
+      margin: 16px;
+      width: 300px; /* HERE*/
+      border: 15px solid darkBlue;
+      padding: 0px;
+      margin: 20px;
       }
-        .card {
-          border-radius: 4px;
-          box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-          overflow: hidden;
-          margin: 16px;
-        }
-        .clone, .name-change, .pic-change{
-          background-color: SaddleBrown;
-          border: 3px solid black;
-          color: white;
-  
-        }
-        .bckr-change, .delete-button{
-          background-color: SaddleBrown;
-          margin: 5px;
-          border: 3px solid black;
-          color: white;
-        }
-        .btn-wrapper {
-         background-color: tan;
-          padding: 10px;
-        }
-        .btn {
-        background-color: SaddleBrown;
-        color: white;
-        font-size: 20px;
-        border-radius: 0%;
-        padding: 14px 40px;
-        width: 80%;
-        margin: 4px 10px 10px 32px;
-        }
-        .btn:focus,.btn:hover {
-        background-color: Moccasin;
-        color: Chocolate;
-        }
-        .card {
-        width: 300px;
-        border: 15px solid darkBlue;
-        padding: 0px;
-        margin: 20px;
+      :host([fancy]) .card {
+      background-color: pink;
+      border: 2px solid brown;
+      box-shadow: 10px 5px 5px red;
+      }
+      .clone, .name-change, .pic-change {
+      background-color: SaddleBrown;
+      border: 3px solid black;
+      color: white;
+      }
+      .bckr-change, .delete-button {
+      background-color: SaddleBrown;
+      margin: 5px;
+      border: 3px solid black;
+      color: white;
+      }
+      .btn-wrapper {
+      background-color: tan;
+      padding: 10px;
+      }
+      .btn, .toggle {
+      background-color: SaddleBrown;
+      color: white;
+      font-size: 20px;
+      border-radius: 0%;
+      padding: 14px 40px;
+      width: 80%;
+      margin: 4px 10px 10px 32px;
+      }
+      .btn:focus, .btn:hover {
+      background-color: Moccasin;
+      color: Chocolate;
+      }
+      .toggle:focus, .toggle:hover {
+      background-color: Moccasin;
+      color: Chocolate;
       }
       h1 {
-  color: Chocolate;
-  margin: 20px
-}
-p {
-  color: SaddleBrown; 
-  margin; 20px
-}
-.change-color{
-  background-color: LightGoldenRodYellow;
-}
-.boarder-color{
-  border: 15px solid DarkSeaGreen;
-}
-
+      color: Chocolate;
+      margin: 20px;
+      }
+      p, .desparh {
+      color: SaddleBrown;
+      margin: 20px;
+      }
+      .change-color {
+      background-color: LightGoldenRodYellow;
+      }
+      .boarder-color {
+      border: 15px solid DarkSeaGreen;
+      }
     `;
   }
-
+  openChanged(e) {
+    console.log(e.newState);
+    if (e.target.open) {
+      this.fancy = true;
+    }
+    else {
+      this.fancy = false;
+    }
+    
+  }
   render() {
-  return html`
+    return html`
     <div class="card">
       <div class="btn-wrapper">
         <h1 class="card-Title">${this.title}</h1>
@@ -98,21 +106,29 @@ p {
         <a href="https://iam.hax.psu.edu/cjs7724/sites/cblog/article-1">
           <button class="btn">Learn more here</button>
         </a>
-        <div id="panel">
+          <!-- when open = toggle,  -->
+        <details ?open="${this.fancy}" @toggle="${this.openChanged}">
+            <button class="toggle">Toggle</button>
+        </details>
+       <!-- <div id="panel">
           <button class="clone">Clone Card</button>
           <button class="name-change">Change name</button>
           <button class="pic-change">Change picture</button>
           <button class="bckr-change">Change background</button>
           <button class="delete-button">Delete button</button>
-        </div>
+        </div> -->
+        <details ?open="${this.fancy}">
+        <summary>Description</summary>
+          <div>
+        <slot>${this.description}</slot>
+          </div>
+        </details>
         <img class="card-image" src="https://upload.wikimedia.org/wikipedia/commons/8/8e/Calliactis_and_Dardanus_001.JPG" style="width: 280px" />
       </div>
     </div>
-    <!-- put this in your render method where you had details -->
-  <details ?open="${this.fancy}" @toggle="${this.openChanged}">
   `;
-}
-       
+  }
+
   static get properties() {
     return {
       title: { type: String },
